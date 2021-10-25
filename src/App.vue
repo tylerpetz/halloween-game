80bfd265d3502d1ce84f029e33aea2c6f878010e
Tyler Petz <tylerp@booj.com>
2021-10-25 14:53:17 -0600

    project setup

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { useDark, useToggle } from '@vueuse/core'
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
+</script>
+
+<template>
+  <button @click="toggleDark" class="bg-gray-400 dark:bg-yellow-600">button</button>
+</template>
